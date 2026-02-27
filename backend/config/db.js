4abea7f490ce5197348c1e8a47bd983ea4186fbb
Rifaya09709 <rifayasafi11@gmail.com>
2026-02-27 14:08:36 +0400

    fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://rifaya:rifaya97@cluster0.ciu9ybk.mongodb.net/authdb");
+    await mongoose.connect("mongodb+srv://rifaya:nashwan21@cluster0.ciu9ybk.mongodb.net/authdb");
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(error.message);
