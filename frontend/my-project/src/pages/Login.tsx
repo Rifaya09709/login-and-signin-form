08dc9b14ee18d0f5e0441c2889b1e794220c4885
Rifaya09709 <rifayasafi11@gmail.com>
2026-03-01 12:16:03 +0400

    Update login API endpoint to production URL

--- frontend/my-project/src/pages/Login.tsx
+++ frontend/my-project/src/pages/Login.tsx
@@ -16,7 +16,7 @@ const Login = () => {
     e.preventDefault();
     try{
     const res = await axios.post(
-      "http://localhost:5000/api/auth/login",
+      "https://login-and-signin-form-21.onrender.com/api/auth/login",
       form
     );
     localStorage.setItem("token", res.data.token);
